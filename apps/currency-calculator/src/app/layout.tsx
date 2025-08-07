9782ebdacb1b6177e89c5a88b82cf72235e1d9ee
Jayant <goyal151002@gmail.com>
2025-08-07 17:31:32 +0530

    updated the code metadata

--- apps/currency-calculator/src/app/layout.tsx
+++ apps/currency-calculator/src/app/layout.tsx
@@ -4,6 +4,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Cash Counter - Currency Calculator",
   description: "Your Personal Money Management Tool",
+  icons: {
+    icon: [
+      { url: '/favicon_io/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon_io/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    shortcut: '/favicon_io/favicon.ico',
+    apple: '/favicon_io/apple-touch-icon.png',
+  },
+  manifest: '/favicon_io/site.webmanifest',
 };
 
 export default function RootLayout({
